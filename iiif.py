4fad24b79008ec0904f51afc277c873f6de4833c
Marc Altmann <marcaltmann@posteo.de>
2024-07-09 14:07:57 +0200

    Work in progress

--- iiif.py
+++ iiif.py
@@ -1,15 +1,15 @@
 from iiif_prezi3 import Manifest, config
 import json, requests
 
-url = 'https://www.loc.gov/item/sn96061150/1889-10-20/ed-1/manifest.json'
+url = "https://www.loc.gov/item/sn96061150/1889-10-20/ed-1/manifest.json"
 
 response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
-    children = data.get('sequences', [])
+    children = data.get("sequences", [])
     for child in children:
-        canvases = child.get('canvases')
+        canvases = child.get("canvases")
         height = []
         width = []
         for canvas in canvases:
@@ -18,65 +18,86 @@ if response.status_code == 200:
         width = max(width)
         height = max(height)
 
-            
-config.configs['helpers.auto_fields.AutoLang'].auto_lang = "en"
 
-manifest = Manifest(id="https://iiif.io/api/cookbook/recipe/0021-tagging/manifest.json",
-                    label={"en": ["The daily morning Astorian. [volume], October 20, 1889"]},
-                    behavior=["paged"])
+config.configs["helpers.auto_fields.AutoLang"].auto_lang = "en"
 
-canvas1 = manifest.make_canvas_from_iiif(url="https://tile.loc.gov/image-services/iiif/service:ndnp:oru:batch_oru_hufnagel_ver02:data:sn96061150:00200298111:1889102001:0368",
-                                         id="https://iiif.io/api/cookbook/recipe/0009-book-1/canvas/p1",
-                                         label="The daily morning Astorian. [volume], October 20, 1889",
-                                         anno_id="https://iiif.io/api/cookbook/recipe/0009-book-1/annotation/p0001-image",
-                                         anno_page_id="https://iiif.io/api/cookbook/recipe/0009-book-1/page/p1/1")
+manifest = Manifest(
+    id="https://iiif.io/api/cookbook/recipe/0021-tagging/manifest.json",
+    label={"en": ["The daily morning Astorian. [volume], October 20, 1889"]},
+    behavior=["paged"],
+)
 
-
-canvas2 = manifest.make_canvas_from_iiif(url="https://tile.loc.gov/image-services/iiif/service:ndnp:oru:batch_oru_hufnagel_ver02:data:sn96061150:00200298111:1889102001:0369",
-                                         id="https://iiif.io/api/cookbook/recipe/0009-book-1/canvas/p2",
-                                         label="The daily morning Astorian. [volume], October 20, 1889",
-                                         anno_id="https://iiif.io/api/cookbook/recipe/0009-book-1/annotation/p0002-image",
-                                         anno_page_id="https://iiif.io/api/cookbook/recipe/0009-book-1/page/p2/1")
-
-anno2 = canvas2.make_annotation(id="https://iiif.io/api/cookbook/recipe/0021-tagging/annotation/p0003-tag",
-                              motivation="tagging",
-                              body={"type": "TextualBody",
-                                    "language": "en",
-                                    "format": "text/plain",
-                                    "value": "Here is another annotation"},
-                              target=canvas2.id + "#xywh=265,661,1260,1239",
-                              anno_page_id="https://www.loc.gov/resource/sn96061150/1889-10-20/ed-1/seq-2/")
-
-canvas3 = manifest.make_canvas_from_iiif(url="https://tile.loc.gov/image-services/iiif/service:ndnp:oru:batch_oru_hufnagel_ver02:data:sn96061150:00200298111:1889102001:0370",
-                                         id="http://127.0.0.1:9200/search/_doc/SZL_J5ABBoOnO3giCKwt",
-                                         label="The daily morning Astorian. [volume], October 20, 1889",
-                                         anno_page_id="https://www.loc.gov/resource/sn96061150/1889-10-20/ed-1/seq-3/")
-
-anno1 = canvas3.make_annotation(id="https://www.loc.gov/resource/sn96061150/1889-10-20/ed-1/seq-3/p0002-tag",
-                              motivation="commenting",
-                              body={"type": "TextualBody",
-                                    "language": "en",
-                                    "format": "text/html",
-                                    "value": "<p>Here is a sample annotation that can link back to a stable url<a href='https://de.wikipedia.org/wiki/G%C3%A4nseliesel-Brunnen_(G%C3%B6ttingen)'>Gänseliesel Brunnen <img src='https://en.wikipedia.org/static/images/project-logos/enwiki.png' alt='Wikipedia logo'></a></p>"},
-                              target=canvas3.id + "#xywh=265,661,1260,1239",
-                              anno_page_id="https://www.loc.gov/resource/sn96061150/1889-10-20/ed-1/seq-3/")
-
-anno2 = canvas3.make_annotation(id="https://www.loc.gov/resource/sn96061150/1889-10-20/ed-1/seq-3/p0003-tag",
-                              motivation="commenting",
-                              body={"type": "TextualBody",
-                                    "language": "en",
-                                    "format": "text/plain",
-                                    "value": "Here is another sample annotation that can link back to a stable <a href=nothing> url</a>"},
-                              target=canvas3.id + "#xywh=2665,1861,160,439",
-                              anno_page_id="https://www.loc.gov/resource/sn96061150/1889-10-20/ed-1/seq-3/")
+canvas1 = manifest.make_canvas_from_iiif(
+    url="https://tile.loc.gov/image-services/iiif/service:ndnp:oru:batch_oru_hufnagel_ver02:data:sn96061150:00200298111:1889102001:0368",
+    id="https://iiif.io/api/cookbook/recipe/0009-book-1/canvas/p1",
+    label="The daily morning Astorian. [volume], October 20, 1889",
+    anno_id="https://iiif.io/api/cookbook/recipe/0009-book-1/annotation/p0001-image",
+    anno_page_id="https://iiif.io/api/cookbook/recipe/0009-book-1/page/p1/1",
+)
 
 
+canvas2 = manifest.make_canvas_from_iiif(
+    url="https://tile.loc.gov/image-services/iiif/service:ndnp:oru:batch_oru_hufnagel_ver02:data:sn96061150:00200298111:1889102001:0369",
+    id="https://iiif.io/api/cookbook/recipe/0009-book-1/canvas/p2",
+    label="The daily morning Astorian. [volume], October 20, 1889",
+    anno_id="https://iiif.io/api/cookbook/recipe/0009-book-1/annotation/p0002-image",
+    anno_page_id="https://iiif.io/api/cookbook/recipe/0009-book-1/page/p2/1",
+)
 
-canvas4 = manifest.make_canvas_from_iiif(url="https://tile.loc.gov/image-services/iiif/service:ndnp:oru:batch_oru_hufnagel_ver02:data:sn96061150:00200298111:1889102001:0371",
-                                         id="https://iiif.io/api/cookbook/recipe/0009-book-1/canvas/p4",
-                                         label="Blank page",
-                                         anno_id="https://iiif.io/api/cookbook/recipe/0009-book-1/annotation/p0004-image",
-                                         anno_page_id="https://iiif.io/api/cookbook/recipe/0009-book-1/page/p4/1")
+anno2 = canvas2.make_annotation(
+    id="https://iiif.io/api/cookbook/recipe/0021-tagging/annotation/p0003-tag",
+    motivation="tagging",
+    body={
+        "type": "TextualBody",
+        "language": "en",
+        "format": "text/plain",
+        "value": "Here is another annotation",
+    },
+    target=canvas2.id + "#xywh=265,661,1260,1239",
+    anno_page_id="https://www.loc.gov/resource/sn96061150/1889-10-20/ed-1/seq-2/",
+)
+
+canvas3 = manifest.make_canvas_from_iiif(
+    url="https://tile.loc.gov/image-services/iiif/service:ndnp:oru:batch_oru_hufnagel_ver02:data:sn96061150:00200298111:1889102001:0370",
+    id="http://127.0.0.1:9200/search/_doc/SZL_J5ABBoOnO3giCKwt",
+    label="The daily morning Astorian. [volume], October 20, 1889",
+    anno_page_id="https://www.loc.gov/resource/sn96061150/1889-10-20/ed-1/seq-3/",
+)
+
+anno1 = canvas3.make_annotation(
+    id="https://www.loc.gov/resource/sn96061150/1889-10-20/ed-1/seq-3/p0002-tag",
+    motivation="commenting",
+    body={
+        "type": "TextualBody",
+        "language": "en",
+        "format": "text/html",
+        "value": "<p>Here is a sample annotation that can link back to a stable url<a href='https://de.wikipedia.org/wiki/G%C3%A4nseliesel-Brunnen_(G%C3%B6ttingen)'>Gänseliesel Brunnen <img src='https://en.wikipedia.org/static/images/project-logos/enwiki.png' alt='Wikipedia logo'></a></p>",
+    },
+    target=canvas3.id + "#xywh=265,661,1260,1239",
+    anno_page_id="https://www.loc.gov/resource/sn96061150/1889-10-20/ed-1/seq-3/",
+)
+
+anno2 = canvas3.make_annotation(
+    id="https://www.loc.gov/resource/sn96061150/1889-10-20/ed-1/seq-3/p0003-tag",
+    motivation="commenting",
+    body={
+        "type": "TextualBody",
+        "language": "en",
+        "format": "text/plain",
+        "value": "Here is another sample annotation that can link back to a stable <a href=nothing> url</a>",
+    },
+    target=canvas3.id + "#xywh=2665,1861,160,439",
+    anno_page_id="https://www.loc.gov/resource/sn96061150/1889-10-20/ed-1/seq-3/",
+)
+
+
+canvas4 = manifest.make_canvas_from_iiif(
+    url="https://tile.loc.gov/image-services/iiif/service:ndnp:oru:batch_oru_hufnagel_ver02:data:sn96061150:00200298111:1889102001:0371",
+    id="https://iiif.io/api/cookbook/recipe/0009-book-1/canvas/p4",
+    label="Blank page",
+    anno_id="https://iiif.io/api/cookbook/recipe/0009-book-1/annotation/p0004-image",
+    anno_page_id="https://iiif.io/api/cookbook/recipe/0009-book-1/page/p4/1",
+)
 
 # # canvas5 = manifest.make_canvas_from_iiif(url="https://iiif.io/api/image/3.0/example/reference/59d09e6773341f28ea166e9f3c1e674f-gallica_ark_12148_bpt6k1526005v_f22",
 # #                                          id="https://iiif.io/api/cookbook/recipe/0009-book-1/canvas/p5",
--- issue_overlay.py
+++ issue_overlay.py
@@ -6,20 +6,21 @@ import json
 import re
 import os
 from dotenv import load_dotenv
-#THIS IS PARTICULAR TO THE INTERNET ARCHIVE FORMAT
+
+# THIS IS PARTICULAR TO THE INTERNET ARCHIVE FORMAT
 
 
 load_dotenv()
-#get the viral texts data for a single issue
-issue_url = 'https://es.viral-texts.software.ncsa.illinois.edu/viral-texts-test/_search?size=100&q=issue:sim_manifesto_1878-05_8_5%20AND%20day:1878-05-01'
+# get the viral texts data for a single issue
+issue_url = "https://es.viral-texts.software.ncsa.illinois.edu/viral-texts-test/_search?size=100&q=issue:sim_manifesto_1878-05_8_5%20AND%20day:1878-05-01"
 
 password = os.environ.get("password")
 username = os.environ.get("username")
 
 issue_json = requests.get(issue_url, auth=(username, password)).json()
 
-#archive.org manifest for the issue in question
-manifest_url = 'https://iiif.archive.org/iiif/3/sim_manifesto_1878-05_8_5/manifest.json'
+# archive.org manifest for the issue in question
+manifest_url = "https://iiif.archive.org/iiif/3/sim_manifesto_1878-05_8_5/manifest.json"
 
 response = requests.get(manifest_url)
 
@@ -28,47 +29,63 @@ data_dict = {}
 CANVAS_WIDTH = 0
 CANVAS_HEIGHT = 0
 
-#add page to canvas
+
+# add page to canvas
 def add_to_canvas(page, canvas_page):
-	page_dict[page] = manifest.make_canvas_from_iiif(url=canvas_page,
-                                         id=canvas_page,
-                                         label="Shaker",
-                                         anno_id=canvas_page,
-                                         anno_page_id=canvas_page)
+    page_dict[page] = manifest.make_canvas_from_iiif(
+        url=canvas_page,
+        id=canvas_page,
+        label="Shaker",
+        anno_id=canvas_page,
+        anno_page_id=canvas_page,
+    )
+
+
 def pct_string_to_xywh(url):
-    pct_x = re.search(r"https:\/\/iiif\.archive\.org\/iiif\/.*\/pct:(.*),(.*),(.*),(.*)\/full", url).group(1)
-    pct_y = re.search(r"https:\/\/iiif\.archive\.org\/iiif\/.*\/pct:(.*),(.*),(.*),(.*)\/full", url).group(1)
-    pct_w = re.search(r"https:\/\/iiif\.archive\.org\/iiif\/.*\/pct:(.*),(.*),(.*),(.*)\/full", url).group(1)
-    pct_h = re.search(r"https:\/\/iiif\.archive\.org\/iiif\/.*\/pct:(.*),(.*),(.*),(.*)\/full", url).group(1)
+    pct_x = re.search(
+        r"https:\/\/iiif\.archive\.org\/iiif\/.*\/pct:(.*),(.*),(.*),(.*)\/full", url
+    ).group(1)
+    pct_y = re.search(
+        r"https:\/\/iiif\.archive\.org\/iiif\/.*\/pct:(.*),(.*),(.*),(.*)\/full", url
+    ).group(1)
+    pct_w = re.search(
+        r"https:\/\/iiif\.archive\.org\/iiif\/.*\/pct:(.*),(.*),(.*),(.*)\/full", url
+    ).group(1)
+    pct_h = re.search(
+        r"https:\/\/iiif\.archive\.org\/iiif\/.*\/pct:(.*),(.*),(.*),(.*)\/full", url
+    ).group(1)
 
     print(float(pct_x))
-    p_x = round(float(pct_x)*.01*CANVAS_WIDTH)
-    p_y = round(float(pct_y)*.01*CANVAS_HEIGHT)
-    p_w = round(float(pct_w)*.01*CANVAS_WIDTH)
-    p_h = round(float(pct_h)*.01*CANVAS_HEIGHT)
+    p_x = round(float(pct_x) * 0.01 * CANVAS_WIDTH)
+    p_y = round(float(pct_y) * 0.01 * CANVAS_HEIGHT)
+    p_w = round(float(pct_w) * 0.01 * CANVAS_WIDTH)
+    p_h = round(float(pct_h) * 0.01 * CANVAS_HEIGHT)
+
+    return "xywh=" + str(p_x) + "," + str(p_y) + "," + str(p_w) + "," + str(p_h)
 
 
-    return("xywh=" + str(p_x) + "," + str(p_y) + "," + str(p_w) + "," + str(p_h))
-
-
-#add annotation to canvas
+# add annotation to canvas
 def add_ann_to_page(page, id, url):
     xywh_string = pct_string_to_xywh(url)
-    page.make_annotation(id=page,
-                                  motivation="tagging",
-                                  body={"type": "TextualBody",
-                                        "language": "en",
-                                        "format": "text/plain",
-                                        "value": "Here is another annotation"},
-                                  target=page.id + xywh_string,
-                                  anno_page_id=page)
+    page.make_annotation(
+        id=page,
+        motivation="tagging",
+        body={
+            "type": "TextualBody",
+            "language": "en",
+            "format": "text/plain",
+            "value": "Here is another annotation",
+        },
+        target=page.id + xywh_string,
+        anno_page_id=page,
+    )
 
 
 if response.status_code == 200:
     manifest_data = response.json()
-    children = manifest_data.get('sequences', [])
+    children = manifest_data.get("sequences", [])
     for child in children:
-        items = child.get('items')
+        items = child.get("items")
         height = []
         width = []
         for item in items:
@@ -80,52 +97,78 @@ if response.status_code == 200:
 CANVAS_WIDTH = manifest_data["items"][0]["width"]
 CANVAS_HEIGHT = manifest_data["items"][0]["height"]
 
-print(pct_string_to_xywh("https://iiif.archive.org/iiif/sim_manifesto_1878-01_8_1$12/pct:51.381215,37.062726,37.338858,26.568154/full/0/default.jpg"))
+print(
+    pct_string_to_xywh(
+        "https://iiif.archive.org/iiif/sim_manifesto_1878-01_8_1$12/pct:51.381215,37.062726,37.338858,26.568154/full/0/default.jpg"
+    )
+)
 
 
-config.configs['helpers.auto_fields.AutoLang'].auto_lang = "en"
+config.configs["helpers.auto_fields.AutoLang"].auto_lang = "en"
 
-manifest = Manifest(id="https://iiif.archive.org/iiif/3/sim_manifesto_1878-05_8_5/manifest.json",
-                    label={"en": ["Shaker blah blah"]},
-                    behavior=["paged"])
+manifest = Manifest(
+    id="https://iiif.archive.org/iiif/3/sim_manifesto_1878-05_8_5/manifest.json",
+    label={"en": ["Shaker blah blah"]},
+    behavior=["paged"],
+)
 
-#populate the data dict
+# populate the data dict
 for id_num, child in enumerate(manifest_data["items"]):
-	canvas_page = child["items"][0]["items"][0]["body"]["service"][0]["id"]
-	data_dict[id_num] = {}
-	data_dict[id_num]["canvas_page"] = canvas_page
-	data_dict[id_num]["annotations"] = []
+    canvas_page = child["items"][0]["items"][0]["body"]["service"][0]["id"]
+    data_dict[id_num] = {}
+    data_dict[id_num]["canvas_page"] = canvas_page
+    data_dict[id_num]["annotations"] = []
 
 
-#add annotations to dict using the issue json
+# add annotations to dict using the issue json
 for cluster_annotation in issue_json["hits"]["hits"]:
-	if cluster_annotation["_source"]["issue"] == "sim_manifesto_1878-05_8_5":
-		archive_url = cluster_annotation["_source"]["url"]
-		page_num = re.search(r"https:\/\/archive\.org\/details\/.*\/page\/n([0-9]*)\/mode", archive_url).group(1)
-		data_dict[int(page_num)]["annotations"].append({"_id": cluster_annotation["_id"], "cluster": cluster_annotation["_source"]["cluster"], "image_data": cluster_annotation["_source"]["page_image"]})
+    if cluster_annotation["_source"]["issue"] == "sim_manifesto_1878-05_8_5":
+        archive_url = cluster_annotation["_source"]["url"]
+        page_num = re.search(
+            r"https:\/\/archive\.org\/details\/.*\/page\/n([0-9]*)\/mode", archive_url
+        ).group(1)
+        data_dict[int(page_num)]["annotations"].append(
+            {
+                "_id": cluster_annotation["_id"],
+                "cluster": cluster_annotation["_source"]["cluster"],
+                "image_data": cluster_annotation["_source"]["page_image"],
+            }
+        )
 
 
-
-
-#dict containing the page manifests
+# dict containing the page manifests
 page_dict = {}
 
 for page in data_dict:
     add_to_canvas(page, data_dict[page]["canvas_page"])
 
+
 for page in data_dict:
     for annotation in data_dict[page]["annotations"]:
-        print(annotation)
-#TODO: add annotations to canvases
-#print(page_dict)
+        url = annotation["image_data"]
+        coords = pct_string_to_xywh(url)
 
-anno1 = page_dict[0].make_annotation(id="https://iiif.io/api/cookbook/recipe/0021-tagging/annotation/p0003-tag",
-                              motivation="tagging",
-                              body={"type": "TextualBody",
-                                    "language": "en",
-                                    "format": "text/plain",
-                                    "value": "Here is another annotation"},
-                              target=page_dict[0].id + "#xywh=265,661,1260,1239",
-                              anno_page_id="https://www.loc.gov/resource/sn96061150/1889-10-20/ed-1/seq-2/")
+        pattern = r"\$(\d{1,3})"
+        regex_page = re.search(pattern, url).group(1)
+        print(page, annotation, coords, regex_page)
+# TODO: add annotations to canvases
+# print(page_dict)
 
-print(manifest.json(indent=2))
+
+for page in page_dict.keys():
+    page_dict[page].make_annotation(
+        id="https://iiif.io/api/cookbook/recipe/0021-tagging/annotation/p0003-tag",
+        motivation="tagging",
+        body={
+            "type": "TextualBody",
+            "language": "en",
+            "format": "text/plain",
+            "value": "Here is another annotation",
+        },
+        target=page_dict[page].id + "#xywh=265,661,1260,1239",
+        anno_page_id="https://www.loc.gov/resource/sn96061150/1889-10-20/ed-1/seq-2/",
+    )
+
+
+with open("output.json", "w") as outfile:
+    outfile.write(manifest.json(indent=2))
